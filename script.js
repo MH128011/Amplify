f8ad9c5a0398bc3fcace3a93af803fb8785de69b
MH128011 <vaishnavidube03@gmail.com>
2025-04-15 14:07:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function showMessage() {
+    const message = document.getElementById('message');
+    message.textContent = "🌟 Hello Vaishnavi! Your pastel site is live and lovely!";
+    message.classList.remove('hidden');
+}
